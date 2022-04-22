6d62c7a08d538a972e1d4ca50bc6d068e038a131
Alejandro Valenzuela <tzalejo@gmail.com>
2022-04-21 22:49:12 -0300

    agrego definicion en la entity

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,16 +1,16 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import {ProductsController } from './controllers/products.controller';
-import {CategoriesController} from './controllers/categories.controller';
-import {ProductsService} from './services/products.service';
-import {TypeOrmModule} from '@nestjs/typeorm';
-import {Product} from './entities/product.entity';
+import { ProductsController } from './controllers/products.controller';
+import { ProductsService } from './services/products.service';
+import { Product } from './entities/product.entity';
+
+import { CategoriesController } from './controllers/categories.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Product])],
-  controllers:[ProductsController, CategoriesController],
-  providers:[ProductsService],
-  exports:[ProductsService]
-
+  controllers: [ProductsController, CategoriesController],
+  providers: [ProductsService],
+  exports: [ProductsService],
 })
 export class ProductsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { UsersService } from './services/users.service';
-import { CustomersService } from './services/customers.service';
-import { CustomersController } from './controllers/customers.controller';
 import { UsersController } from './controllers/users.controller';
+import { UsersService } from './services/users.service';
+import { User } from './entities/user.entity';
+
+import { CustomersController } from './controllers/customers.controller';
+import { CustomersService } from './services/customers.service';
+import { Customer } from './entities/customer.entity';
 
 import { ProductsModule } from '../products/products.module';
+import { Order } from './entities/order.entity';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [TypeOrmModule.forFeature([ProductsModule, Customer, User, Order])],
   controllers: [CustomersController, UsersController],
   providers: [CustomersService, UsersService],
 })
